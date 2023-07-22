1b8a596f179ed5ba48900091be07a2c1af449840
simon <dlgmlals3@gmail.com>
2023-07-22 20:50:28 +0900

    init

---
+++ ConsoleApp1/ProductFilter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersn
+{
+	public enum Color
+	{
+		Red, Green, Blue
+	}
+
+	public enum Size
+	{
+		Small, Medium, Large, Yuge
+	}
+
+	public class Product
+	{
+		public string Name;
+		public Color Color;
+		public Size Size;
+
+
+		public Product(string name, Color color, Size size)
+		{
+			if (name == null)
+			{
+				throw new ArgumentNullException(paramName: nameof(name));
+			}
+			Name = name;
+			Color = color;
+			Size = size;
+		}
+	}
+
+	public class ProductFilter
+	{
+		public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+		{
+			foreach (var p in products)
+			{
+				if (p.Size == size)
+					yield return p;
+			}
+		}
+		public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+		{
+			foreach (var p in products)
+			{
+				if (p.Color == color)
+					yield return p;
+			}
+		}
+		public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+		{
+			foreach (var p in products)
+			{
+				if (p.Size == size && p.Color == color)
+					yield return p;
+			}
+		}
+	}
+}
---
+++ ConsoleApp1/journal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace DesignPattersn
+{
+	public class journal
+	{
+		private readonly List<string> entries = new List<string>();
+		private static int count = 0;
+		public int AddEntry(string text)
+		{
+			entries.Add($"{++count}:{text}");
+			return count;
+		}
+
+		public void RemoveEntry(int index)
+		{
+			entries.RemoveAt(index);
+		}
+		public override string ToString()
+		{
+			return string.Join(Environment.NewLine, entries);
+		}
+	}
+
+	public class Persistence
+	{
+		public void SaveToFile(journal j, string filename, bool overwrite = true)
+		{
+			if (overwrite || !File.Exists(filename))
+			{
+				File.WriteAllText(filename, j.ToString());
+			}
+		}
+		public void Save(string filename)
+		{
+			File.WriteAllText(filename, ToString());
+		}
+	}
+}
